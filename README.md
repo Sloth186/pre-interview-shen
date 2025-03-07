11f35aa2a52064a5974411037ccd2e27968dc9b4
SCRoth <rother2@stolaf.edu>
2025-03-07 13:03:02 -0600

    Finished task 4 and updated README.md

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Shen Rothermel
 I have been to 9 different schools through K-12.
 
 For Task 3, run "./run-queries.sh [...]" where [...] is replaced by the directory where all the queries are contained.
+
+For Task 4, run "./run-queries-data.sh [...]" where [...] is replaced by the directory where all the queries are contained. After execution finishes, a text file named data should be produced. Open this for the desired information.
--- run-queries-data.sh
+++ run-queries-data.sh
@@ -1,19 +1,38 @@
 #! /usr/local/bin/bash
 DIR=$@
-echo "Running cvc5 on all files in $DIR"
+resultre="(unknown|sat|unsat)"
+queryre="driver::filename = queries\/([a-zA-Z0-9_\/\.-]+)"
+benchmarkre="global::totalTime = ([0-9]+)ms"
+printf "QueryName,Result,ElapsedTime\n" &> data.txt
 for file in "$DIR"/*
 do
-    echo "Checking $file..."
-    time output=$(./cvc5 "$file" --tlimit=2000 --output=raw-benchmark)
-    if [[ "$output" == "" ]]
-    then
-        echo "TIMEOUT"
-    else
-        if [[ "$output" == "sat" ]]
-        then
-            echo "SAT"
+    printf "$(basename $file)\n"
+    result="null"
+    query="null"
+    benchmark="null"
+    (./cvc5 $file --tlimit-per=1000 --stats) &> output.txt
+    while read line
+    do
+        if [[ $line =~ $resultre ]]; then
+            result="${BASH_REMATCH[1]}"
+            if [[ $result == "unknown" ]]; then
+                result="TIMEOUT"
+            else
+                if [[ $result == "unsat" ]]; then
+                    result="UNSAT"
+                else
+                    result="SAT"
+                fi
+            fi
         else
-            echo "UNSAT"
+            if [[ $line =~ $queryre ]]; then
+                query="${BASH_REMATCH[1]}"
+            else
+                if [[ $line =~ $benchmarkre ]]; then
+                    benchmark="${BASH_REMATCH[1]}\n"
+                fi
+            fi
         fi
-    fi
+    done <<< "$(cat output.txt)"
+    printf "$query,$result,$benchmark" &>> data.txt
 done
